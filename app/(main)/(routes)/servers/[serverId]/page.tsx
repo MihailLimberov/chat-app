13006f81aa6a2b6f57d929119b2cfc86addf78a0
ELEMAG <m.d.limberov@gmail.com>
2025-12-04 16:19:02 +0200

    created channels/members pages #26

---
+++ app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -0,0 +1,51 @@
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { RedirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+interface ServerIdPageProps {
+    params: {
+        serverId: string;
+    }
+};
+
+const ServerIdPage = async ({
+    params,
+}: ServerIdPageProps) => {
+
+    const profile = await currentProfile();
+    const {serverId} = await params;
+
+    if (!profile) {
+        return RedirectToSignIn;
+    }
+
+    const server = await db.server.findFirst({
+        where: {
+            id: serverId,
+            members: {
+                some: {
+                    profileId: profile.id,
+                }
+            }
+        },
+        include: {
+            channels: {
+                where: {
+                    name: "general",
+                },
+                orderBy: {
+                    createdAt: "asc",
+                }
+            }
+        }
+    });
+
+    const initialChannel = server?.channels[0];
+
+    if (initialChannel?.name !== "general") return null;
+
+    return redirect(`/servers/${serverId}/channels/${initialChannel?.id}`);
+}
+
+export default ServerIdPage;
